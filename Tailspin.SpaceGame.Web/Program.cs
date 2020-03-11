ca4bc1266c2a496edb6ba5ada1ebbac7d86f6940
Nikita kale <kalenikita61@gmail.com>
2020-03-11 10:47:20 +0530

    commented in Program cs file

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -10,8 +10,8 @@ namespace TailSpin.SpaceGame.Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+       // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+       //     WebHost.CreateDefaultBuilder(args)
+       //         .UseStartup<Startup>();
     }
 }
